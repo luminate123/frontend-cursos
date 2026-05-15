5f839555c49a62375153790fb12e08857eb9ceaa
luminate123 <ssenmache8@gmail.com>
2026-05-15 09:34:59 -0500

    initial commit: Next.js frontend EduTech Pro

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import type { NextConfig } from "next";
+import path from 'path'
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	/* config options here */
-};
+  outputFileTracingRoot: path.join(__dirname, '../'),
+}
 
-export default nextConfig;
-
-// Enable calling `getCloudflareContext()` in `next dev`.
-// See https://opennext.js.org/cloudflare/bindings#local-access-to-bindings.
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-initOpenNextCloudflareForDev();
+export default nextConfig
